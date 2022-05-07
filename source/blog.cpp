cd5965221c541fbb493c72ccee311039ce231938
natanhp <natanhp@protonmail.com>
2022-05-07 15:31:23 +0700

    Add favicon
    Signed-off-by: natanhp <natanhp@protonmail.com>

--- source/blog.cpp
+++ source/blog.cpp
@@ -1,5 +1,6 @@
 #include <Wt/WApplication.h>
 #include <Wt/WBootstrap5Theme.h>
+#include <WServer.h>
 
 class BlogApplication : public Wt::WApplication {
 public:
@@ -14,7 +15,17 @@ BlogApplication::BlogApplication(const Wt::WEnvironment &env) : Wt::WApplication
 
 
 int main(int argc, char **argv) {
-    return Wt::WRun(argc, argv, [](const Wt::WEnvironment &env) {
-        return std::make_unique<BlogApplication>(env);
-    });
+    Wt::WServer server{argc, argv, WTHTTP_CONFIGURATION};
+
+    server.addEntryPoint(Wt::EntryPointType::Application,
+                         [&](const Wt::WEnvironment &env) {
+                             return std::make_unique<BlogApplication>(env);
+                         },
+                         "\0",
+                         "resources/icons/favicon.ico"
+    );
+
+    server.run();
+
+    return 0;
 }
