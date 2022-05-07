d6d0f37a015f0b936bfff93fdebf8fc8f6a82d75
natanhp <natanhp@protonmail.com>
2022-05-07 17:13:48 +0700

    Remove unused code
    Signed-off-by: natanhp <natanhp@protonmail.com>

--- source/blog.cpp
+++ source/blog.cpp
@@ -7,7 +7,6 @@
 #include <Wt/WMessageBox.h>
 #include <Wt/WNavigationBar.h>
 #include <Wt/WPopupMenu.h>
-#include <Wt/WPopupMenuItem.h>
 #include <Wt/WStackedWidget.h>
 #include <Wt/WText.h>
 #include "blog.h"
@@ -28,50 +27,11 @@ BlogApplication::BlogApplication(const Wt::WEnvironment &env) : Wt::WApplication
     auto leftMenu = std::make_unique<Wt::WMenu>(contentsStack);
     auto leftMenu_ = navigation->addMenu(std::move(leftMenu));
 
-    leftMenu_->addItem("Home", std::make_unique<ProfilePage>());
-    leftMenu_->addItem("Layout", std::make_unique<Wt::WText>("Layout contents"))
-            ->setLink(Wt::WLink(Wt::LinkType::InternalPath, "/layout"));
+    leftMenu_->addItem("Home", std::make_unique<ProfilePage>())
+            ->setLink(Wt::WLink(Wt::LinkType::InternalPath, "/"));
     leftMenu_->addStyleClass("me-auto");
 
-// Add a Search control.
-    auto editPtr = std::make_unique<Wt::WLineEdit>();
-    auto edit = editPtr.get();
-    edit->setPlaceholderText("Enter a search item");
 
-    edit->enterPressed().connect([=] {
-        leftMenu_->select(2); // is the index of the "Sales"
-    });
-
-    navigation->addSearch(std::move(editPtr));
-
-// Setup a Right-aligned menu.
     auto rightMenu = std::make_unique<Wt::WMenu>();
     auto rightMenu_ = navigation->addMenu(std::move(rightMenu));
-
-// Create a popup submenu for the Help menu.
-    auto popupPtr = std::make_unique<Wt::WPopupMenu>();
-    auto popup = popupPtr.get();
-    popup->addItem("Contents");
-    popup->addItem("Index");
-    popup->addSeparator();
-    popup->addItem("About");
-
-    popup->itemSelected().connect([=](Wt::WMenuItem *item) {
-        auto messageBox = popup->addChild(
-                std::make_unique<Wt::WMessageBox>
-                        ("Help",
-                         Wt::WString("<p>Showing Help: {1}</p>").arg(item->text()),
-                         Wt::Icon::Information,
-                         Wt::StandardButton::Ok));
-
-        messageBox->buttonClicked().connect([=] {
-            popup->removeChild(messageBox);
-        });
-
-        messageBox->show();
-    });
-
-    auto item = std::make_unique<Wt::WMenuItem>("Help");
-    item->setMenu(std::move(popupPtr));
-    rightMenu_->addItem(std::move(item));
 }
