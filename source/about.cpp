584c0c60d6a3e35ba0cb78d3ac1345d7b44212c0
natanhp <natanhp@protonmail.com>
2022-05-08 06:15:11 +0700

    Update temporary under construction site
    Signed-off-by: natanhp <natanhp@protonmail.com>

--- source/about.cpp
+++ source/about.cpp
@@ -4,7 +4,37 @@
 
 #include "about.h"
 #include <Wt/WText.h>
+#include <Wt/WVBoxLayout.h>
+#include <Wt/WImage.h>
+#include <Wt/WLink.h>
 
 AboutPage::AboutPage() : Wt::WContainerWidget() {
-    this->addNew<Wt::WText>("Under construction.");
+    this->setStyleClass("mx-5 my-3 px-5");
+    auto vbox = this->setLayout(std::make_unique<Wt::WVBoxLayout>());
+
+    auto item = std::make_unique<Wt::WText>("Site Is Under Construction ^_^");
+    item->setStyleClass("h2 text-center mb-4");
+    vbox->addWidget(std::move(item));
+
+    item = std::make_unique<Wt::WText>(
+            "<p>Unfortunately this website is still under construction. "
+            "Here's a picture of Kojima Kana to cure your disappointment :).</p>");
+    item->setStyleClass("text-start");
+    vbox->addWidget(std::move(item));
+
+    auto image = std::make_unique<Wt::WImage>(Wt::WLink("resources/images/kojima-kana.webp"));
+    image->setAlternateText("Kojima Kana");
+    image->setMaximumSize(350, Wt::WLength::Auto);
+    image->setStyleClass("img-fluid img-thumbnail mx-auto mb-3");
+    vbox->addWidget(std::move(image));
+
+    item = std::make_unique<Wt::WText>("<p>I'm working on this project while you're staring at her. "
+                                       "After I deleted my never-finished projects in various frameworks and "
+                                       "architecture, I finally (hopefully can) stick to this project that I write in C++."
+                                       " I hope I can finish it this time without switching to another framework or "
+                                       "architecture. DM me on Twitter "
+                                       "<a href=\"https://twitter.com/Natan_____\" target=\"_blank\">@Natan_____</a> "
+                                       "if you have any suggestions. Thank you.</p>");
+    item->setStyleClass("text-start");
+    vbox->addWidget(std::move(item));
 }
