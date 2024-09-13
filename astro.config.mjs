7ab6c4c9d831697b2e6e5c049f26409e4840185b
Natan Hari Pamungkas <natanhp@protonmail.com>
2024-09-14 03:18:24 +0700

    Remove slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://natanhp.github.io/',
+	site: 'https://natanhp.github.io',
 	markdown: {
 		syntaxHighlight: false,
 		// Disable syntax built-in syntax hightlighting from astro
