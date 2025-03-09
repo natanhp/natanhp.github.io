d64407f9262343a6897ac653f5cd576fca24349f
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-03-09 21:33:25 +0700

    Add icons
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/blogs/renungan_puasa_bagi_anak_muda.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Frenungan_puasa_bagi_anak_muda.mdx&astroContentModuleFlag=true")]]);
+		
--- src/content.config.js
+++ src/content.config.js
@@ -2,7 +2,7 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blogCollection = defineCollection({
-	loader: glob({ pattern: '*.md', base: './src/blogs' }),
+	loader: glob({ pattern: '*.{md,mdx}', base: './src/blogs' }),
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
