57b93e06deae9a5ac19ff2df56116317cea77017
Natan Hari Pamungkas <natanhp@protonmail.com>
2025-03-15 07:39:14 +0700

    Add post flare on
    Signed-off-by: Natan Hari Pamungkas <natanhp@protonmail.com>

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/blogs/flare-on-11-1-frog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Fflare-on-11-1-frog.mdx&astroContentModuleFlag=true")],
 ["src/blogs/renungan_puasa_bagi_anak_muda.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fblogs%2Frenungan_puasa_bagi_anak_muda.mdx&astroContentModuleFlag=true")]]);
 		
